a789eb248941e200f26eca49fb2b48ffd75a8136
eckysaroyd <enyato98@gmail.com>
2019-08-22 11:59:42 +0530

    new program

---
+++ ForLoop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+   public class Program
+    {
+       public  static void Main(string[] args)
+        {
+            for(int i=1;  i<=10; i++)
+            { 
+            Console.WriteLine(i);
+           
+            }
+        Console.ReadKey();
+        } 
+    }
